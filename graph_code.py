f48f65786b380abc8abe57c12ebfe945829a818d
mnoorfawi <m.noor.fawi@gmail.com>
2020-04-06 23:16:47 +0400

    graph traversals with pytholog

--- graph_code.py
+++ graph_code.py
@@ -1,16 +1,3 @@
-# Finding paths in a graph using pytholog
-
-Will use [pytholog](github.com/mnoorfawi/pytholog) library.
-
-We will define a **weighted undirected graph** for the largest MSAs in USA.
-
-![](/images/msa_usa.png)
-
-###### Image and examples source is ""Classic Computer Science Problems in Python" book.
-
-We first import the library and define the nodes and edges of the graph as prolog facts and rules.
-
-```python
 import pytholog as pl
 
 graph_kb = pl.knowledge_base("MSA_graph")
@@ -48,83 +35,53 @@ graph_kb([## routes between adjacent cities
     #"path(X, Y, P) :- route(Y, X, P)",
     "path(X, Y, P) :- route(Y, Z, P2), path(Z, X, P3), P is P2 + P3"
         ])
-```
-
-We only needed to define the facts, the route between a city and its adjacent cities and the distance between them, 
-then define the rule to traverse the graph searching for the path. 
-Now let's search for some paths between some cities.
-
-###### pytholog uses Breadth-First Search algorithm to search for paths, So not always the result will be the shortest path but most of the cases it is. One more things the visited note returned when show_path = True, can be more than the one used to calculate the weight reponse, this is because the bfs search checks all nodes next to the current node and return it as visited, but you can see the actual path from the image which will lead to the result weight. Future implementations will support other kind of search algorithms. 
-
-```python
+        
 x, y = graph_kb.query(pl.pl_expr("path(boston, miami, Weight)"), cut = True, show_path = True) ## cut argument to stop searching when a path is found
 print(x)
 print([x for x in y if str(x) > "Z"]) ## remove weights in the visited nodes
 
 # [{'Weight': 1317}]
 # ['washington', 'new_york', 'philadelphia']
-```
 
-The shortes possible path between the two cities!
-###### N.B. The path given isn't sorted.
-
-```python
 ## the other way
 x, y = graph_kb.query(pl.pl_expr("path(miami, boston, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': 1317}]
 # ['new_york', 'washington', 'philadelphia']
-```
 
-```python
 x, y = graph_kb.query(pl.pl_expr("path(seattle, washington, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': 2371}]
 # ['chicago', 'detroit']
-```
 
-```python
 x, y = graph_kb.query(pl.pl_expr("path(san_francisco, atlanta, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': 2678}]
 # ['houston', 'dallas', 'riverside', 'chicago']
-``` 
 
-Note here the weight is the second shortest path but it can be enhanced by better defining the facts and rules.
-Note also that the path given show "Houston & Dallas" but if you calculate the weights you will find that the algorithm never passed by them. It only passed to Riverside then Chicago then Atlanta.
-But the value is given because they were checked.
-
-```python
 x, y = graph_kb.query(pl.pl_expr("path(chicago, detroit, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': '238'}]
 # []
-```
 
-```python
 x, y = graph_kb.query(pl.pl_expr("path(los_angeles, dallas, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': 1244}]
 # ['phoenix']
-```
 
-```python
 x, y = graph_kb.query(pl.pl_expr("path(riverside, washington, Weight)"), cut = True, show_path = True)
 print(x)
-[x for x in y if str(x) > "Z"]
+print([x for x in y if str(x) > "Z"])
 
 # [{'Weight': 2338}]
 # ['miami', 'chicago', 'atlanta', 'detroit']
-```
-
-The lowest weight was given but again "Miami" was returned although when calculating the weights we will find out that the algorithm never passed by it but it checked it because of the return rule defined.
